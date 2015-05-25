71f85f5a6347f03272ba77fd92dc0660de6477ca
Kevin Qiu <kevin@freshbooks.com>
2015-05-25 00:52:01 -0400

    Read requirements files for setup.py

--- setup.py
+++ setup.py
@@ -14,12 +14,11 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
 
-requirements = [
-    # TODO: put package requirements here
-]
+requirements = open('requirements.txt').readlines()
 
 test_requirements = [
-    # TODO: put package test requirements here
+    line for line in open('requirements-dev.txt').readlines()
+    if not line.startswith('-')
 ]
 
 setup(
