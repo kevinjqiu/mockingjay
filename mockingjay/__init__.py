24c1277349ad4c6c9d782edfed85e787c98900f0
Kevin Qiu <kevin@freshbooks.com>
2015-05-27 01:12:21 -0400

    Version bump

--- mockingjay/__init__.py
+++ mockingjay/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Kevin J. Qiu'
 __email__ = 'kevin@idempotent.ca'
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 
 
 from .service import MockService  # noqa
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='mockingjay',
-    version='0.1.1',
+    version='0.2.0',
     description="A simple library to build mock http services based on HTTPretty",
     long_description=readme + '\n\n' + history,
     author="Kevin J. Qiu",
